8e996319899a60c46e95009203090083f4c4ae06
Lukasz Pudlo <l.pudlo1990@gmail.com>
2026-01-30 11:21:46 +0000

    Add models to admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book, Author, Publication, PublishingHouse
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'reader',
+                    'created', 'updated', 'status']
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'created', 'updated']
+
+
+@admin.register(Publication)
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ['author', 'book', 'publisher', 'created', 'updated']
+
+
+@admin.register(PublishingHouse)
+class PublishingHouseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'location', 'established', 'created', 'updated']
