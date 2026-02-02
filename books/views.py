88dc98990854aedf1054dcb00bffa2ec1319c99b
Lukasz Pudlo <l.pudlo1990@gmail.com>
2026-02-02 23:20:30 +0000

    Add list and detail views

--- books/views.py
+++ books/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Book
+
+
+def book_list(request):
+    books = Book.objects.all()
+
+    return render(
+        request,
+        'books/list.html',
+        {'books': books}
+    )
+
+
+def book_detail(request, id):
+    book = get_object_or_404(
+        Book,
+        id=id
+    )
+
+    return render(
+        request,
+        'books/detail.html',
+        {'book': book}
+    )
