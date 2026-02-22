943aea41ca655ee56b84d1080db2d98d68e3744c
Lukasz Pudlo <l.pudlo1990@gmail.com>
2026-02-22 23:57:08 +0000

    Remove django-tables2 and bootstrap5

--- books/tables.py
+++
@@ -1,9 +0,0 @@
-from django_tables2 import tables
-
-from .models import Book
-
-
-class BookTable(tables.Table):
-    class Meta:
-        model = Book
-        fields = ('title', 'edition', 'status')
--- books/views.py
+++ books/views.py
@@ -1,21 +1,17 @@
-from django_tables2 import RequestConfig
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
 
 from .models import Book
-from .tables import BookTable
 
 
 @login_required
 def book_list(request):
-    table = BookTable(Book.objects.filter(reader=request.user),
-                      template_name="tables/bootstrap_htmx.html")
-    RequestConfig(request, paginate={"per_page": 10}).configure(table)
+    books = Book.objects.filter(reader=request.user)
 
     return render(
         request,
         'books/list.html',
-        {'table': table}
+        {'books': books}
     )
 
 
