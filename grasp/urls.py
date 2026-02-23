b1cee43872ea40e63f6a61b2e12339ac54b7d558
Lukasz Pudlo <l.pudlo1990@gmail.com>
2026-02-23 23:27:14 +0000

    Add index page

--- grasp/urls.py
+++ grasp/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from grasp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('books.urls', namespace='books')),
+    path('', views.index, name='index'),
 ]
