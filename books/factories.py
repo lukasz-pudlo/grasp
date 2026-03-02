18997784fa52bcc6a6d968c6c007101089730a39
Lukasz Pudlo <l.pudlo1990@gmail.com>
2026-03-02 23:33:31 +0000

    Add model factories

--- books/factories.py
+++ books/factories.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import factory
 from books.models import Book, Author, Publication, PublishingHouse, User
 
@@ -26,6 +27,36 @@ class BookFactory(factory.django.DjangoModelFactory):
         model = Book
 
     title = factory.Faker("catch_phrase")
+    edition = factory.Faker("random_digit_not_null")
+    reader = factory.SubFactory(UserFactory)
+    status = factory.Faker(
+        "random_element",
+        elements=[
+            x[0] for x in Book.Status
+        ]
+    )
+    isbn = factory.Faker("random_number", digits=13)
+
+    @factory.post_generation
+    def languages(self, create, extracted, **kwargs):
+        if not create or not extracted:
+            return
+
+        self.languages.add(*extracted)
+
+
+class PublishingHouseFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        user = PublishingHouse
+
+    country = factory.Iterator(["PL", "UK", "DE", "FR"])
+    name = factory.Faker("company")
+    address = factory.Faker("address")
+    established = factory.Faker(
+        "date_between",
+        start_date=datetime(year=1900, month=1, day=1),
+        end_date=datetime.now().date()
+    )
 
 
 class PublicationFactory(factory.django.DjangoModelFactory):
@@ -33,3 +64,11 @@ class PublicationFactory(factory.django.DjangoModelFactory):
         user = Publication
 
     author = factory.SubFactory(AuthorFactory)
+    book = factory.SubFactory(BookFactory)
+    publisher = factory.SubFactory(PublishingHouseFactory)
+
+
+class BookLanguageFactory(factory.django.DjangoModelFactory):
+    name = factory.Iterator(
+        ["British English", "American English", "Spanish (Spain)", "French (France)"])
+    # How to make the tag correspond to the name, e.g. en-GB for British English?
