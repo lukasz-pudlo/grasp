097cfe31a03ae7b80dc4212d4baa5e882cda5039
Lukasz Pudlo <l.pudlo1990@gmail.com>
2026-03-02 00:49:45 +0000

    Add UserFactory

---
+++ books/factories.py
@@ -0,0 +1,12 @@
+import factory
+from books.models import Book, Author, Publication, PublishingHouse, User
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+        django_get_or_create = ("username",)
+
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    username = factory.Sequence(lambda n: "user%d" % n)
