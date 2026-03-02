d5ab045e71bf5aff9a64219b01fe0ffc7a55bd7b
Lukasz Pudlo <l.pudlo1990@gmail.com>
2026-03-02 09:30:42 +0000

    Start adding AuthorFactory, BookFactory, PublicationFactory

--- books/factories.py
+++ books/factories.py
@@ -16,3 +16,20 @@ class AuthorFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Author
         django_get_or_create = ("full_name")
+
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+
+
+class BookFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Book
+
+    title = factory.Faker("catch_phrase")
+
+
+class PublicationFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        user = Publication
+
+    author = factory.SubFactory(AuthorFactory)
