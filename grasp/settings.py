383af6b491d7c431f797a4ba41a668244112ad99
Lukasz Pudlo <l.pudlo1990@gmail.com>
2026-03-06 00:50:54 +0000

    Reinstate django-toolbar settings

--- grasp/settings.py
+++ grasp/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
     'django_filters',
     'widget_tweaks',
     'django_htmx',
-    'debug_toolbar',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -58,6 +58,18 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "django_htmx.middleware.HtmxMiddleware",
 ]
+
+TESTING = "test" in sys.argv or "PYTEST_VERSION" in os.environ
+
+if not TESTING:
+    INSTALLED_APPS = [
+        *INSTALLED_APPS,
+        "debug_toolbar",
+    ]
+    MIDDLEWARE = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+        *MIDDLEWARE,
+    ]
 
 ROOT_URLCONF = 'grasp.urls'
 
--- grasp/urls.py
+++ grasp/urls.py
@@ -25,3 +25,11 @@ urlpatterns = [
     path('books/', include('books.urls', namespace='books')),
     path('', views.index, name='index'),
 ]
+
+
+if not settings.TESTING:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+
+    urlpatterns = [
+        *urlpatterns,
+    ] + debug_toolbar_urls()
