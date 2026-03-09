d2fa31e9976fdaf04d475e9656ed5c9a87d07b21
Lukasz Pudlo <l.pudlo1990@gmail.com>
2026-03-09 22:37:20 +0000

    Associate test books with one user

--- books/tests/conftest.py
+++ books/tests/conftest.py
@@ -1,7 +1,13 @@
 import pytest
-from books.factories import PublishingHouseFactory
+from books.factories import PublishingHouseFactory, BookFactory, UserFactory
 
 
 @pytest.fixture
 def publishing_houses():
     return PublishingHouseFactory.create_batch(50)
+
+
+@pytest.fixture
+def user_books():
+    user = UserFactory()
+    return BookFactory.create_batch(100, reader=user)
