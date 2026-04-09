bc92a31291243c60a2139610ce4e8131fb3b8746
Lukasz Pudlo <l.pudlo1990@gmail.com>
2026-04-09 22:46:46 +0100

    Allow to edit title and edition of books

--- books/forms.py
+++ books/forms.py
@@ -5,10 +5,10 @@ from books.models import Book
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ("status", )
+        fields = ("title", "authors", "edition", "status",)
 
 
 class BookAddForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ("title", "edition", "status", "languages")
+        fields = ("title", "edition", "status", "languages",)
